41437ac2aa2a6a5210d2b31b48cf73cc5b1a9c39
Adam Gardner <Adam.Gardner@dynatrace.com>
2020-09-25 15:45:35 +1000

    Add GET support

--- dynatrace-maintenance-window/maintenance-window.sh
+++ dynatrace-maintenance-window/maintenance-window.sh
@@ -22,51 +22,74 @@ echo "Running container"
 # Required parameters
 DYNATRACE_BASE_URL=${DYNATRACE_BASE_URL:?'DYNATRACE_BASE_URL variable missing.'}
 DYNATRACE_API_TOKEN=${DYNATRACE_API_TOKEN:?'DYNATRACE_API_TOKEN variable missing.'}
-NAME=${NAME?'NAME variable missing.'}
-DESCRIPTION=${DESCRIPTION?'DESCRIPTION variable missing.'}
-TYPE=${TYPE?'TYPE variable missing. Must be one of [PLANNED, UNPLANNED]'}
-SUPPRESSION=${SUPPRESSION?'SUPPRESSION variable missing. Must be one of [ DETECT_PROBLEMS_AND_ALERT, DETECT_PROBLEMS_DONT_ALERT, DONT_DETECT_PROBLEMS ]'}
-SCOPE=${SCOPE?'SCOPE variable missing. Please provide a scope for this maintenance window.'}
-SCHEDULE=${SCHEDULE?'SCHEDULE variable missing.'}
-
+METHOD=${METHOD:?'METHOD variable is missing. Must be one of [GET, POST]'}
 
 # Calculated values
 DYNATRACE_API_URL="$DYNATRACE_BASE_URL/api/config/v1/maintenanceWindows"
 
-# build up POST_BODY variable
-POST_BODY="{\"name\":\"${NAME}\""
-POST_BODY+=", \"description\":\"${DESCRIPTION}\""
-POST_BODY+=", \"type\":\"${TYPE}\""
-POST_BODY+=", \"suppression\":\"${SUPPRESSION}\""
-POST_BODY+=", \"scope\": ${SCOPE}"
-POST_BODY+=", \"schedule\": ${SCHEDULE}"
-# add the closing bracket
-POST_BODY="${POST_BODY}}"
-
 if [[ "${DEBUG}" == "true" ]]; then
-  ARGS+=( --verbose )
+    ARGS+=( --verbose )
+  fi
+
+############################
+# GET LOGIC
+############################
+if [[ "${METHOD}" == "GET" ]]; then
+  ARGS=(
+    --request GET
+    --silent
+    --url "${DYNATRACE_API_URL}"
+    --header "Content-type: application/json"
+    --header "Authorization: Api-Token ${DYNATRACE_API_TOKEN}"
+  )
 fi
 
-ARGS=(
-  --request POST
-  --silent
-  --url "${DYNATRACE_API_URL}"
-  --header "Content-type: application/json"
-  --header "Authorization: Api-Token ${DYNATRACE_API_TOKEN}"
-  --data "${POST_BODY}"
-)
+############################
+# POST LOGIC
+############################
+
+if [[ "${METHOD}" == "POST" ]]; then
+  NAME=${NAME?'NAME variable missing.'}
+  DESCRIPTION=${DESCRIPTION?'DESCRIPTION variable missing.'}
+  TYPE=${TYPE?'TYPE variable missing. Must be one of [PLANNED, UNPLANNED]'}
+  SUPPRESSION=${SUPPRESSION?'SUPPRESSION variable missing. Must be one of [ DETECT_PROBLEMS_AND_ALERT, DETECT_PROBLEMS_DONT_ALERT, DONT_DETECT_PROBLEMS ]'}
+  SCOPE=${SCOPE?'SCOPE variable missing. Please provide a scope for this maintenance window.'}
+  SCHEDULE=${SCHEDULE?'SCHEDULE variable missing.'}
+
+  # build up POST_BODY variable
+  POST_BODY="{\"name\":\"${NAME}\""
+  POST_BODY+=", \"description\":\"${DESCRIPTION}\""
+  POST_BODY+=", \"type\":\"${TYPE}\""
+  POST_BODY+=", \"suppression\":\"${SUPPRESSION}\""
+  POST_BODY+=", \"scope\": ${SCOPE}"
+  POST_BODY+=", \"schedule\": ${SCHEDULE}"
+  # add the closing bracket
+  POST_BODY="${POST_BODY}}"
+
+  ARGS=(
+    --request POST
+    --silent
+    --url "${DYNATRACE_API_URL}"
+    --header "Content-type: application/json"
+    --header "Authorization: Api-Token ${DYNATRACE_API_TOKEN}"
+    --data "${POST_BODY}"
+  )
+fi
 
 echo "----------------------------------------------------"
 echo "Calling Dynatrace Maintenance Window API"
 echo "DYNATRACE_BASE_URL = ${DYNATRACE_BASE_URL}"
-echo "POST_BODY          = $POST_BODY"
+echo "METHOD = ${METHOD}"
+if [[ "${METHOD}" == "POST" ]]; then
+  echo "POST_BODY = $POST_BODY"
+fi
 echo "----------------------------------------------------"
 status=$(curl "${ARGS[@]}")
 echo "API call results = $status"
 echo "----------------------------------------------------"
 if [[ "$status" == *"error"* ]]; then
-  echo "Error: Failed sending Dynatrace Maintenance Window: $status"
+  echo "Error: Failed: $status"
   exit 1
 else
-  echo "Success: Sending Dynatrace Maintenance Window: $status"
+  echo "Success: $status"
 fi
